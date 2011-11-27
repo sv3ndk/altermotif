75c57482410d75d3b1c05bf1a62c654185de3c16
Svend Vanderveken <svend.vanderveken@gmail.com>
2011-11-27 23:19:37 +0100

    view project page, with list of locations

--- app/controllers/projects/ProjectsView.java
+++ app/controllers/projects/ProjectsView.java
@@ -1,11 +1,22 @@
 package controllers.projects;
 
+import web.utils.Utils;
+
+import com.google.common.base.Strings;
+
+import controllers.BeanProvider;
 import controllers.DabController;
 
 public class ProjectsView extends DabController{
 
 	
 	public static void projectsView(String vp) {
+		if (!Strings.isNullOrEmpty(vp)) {
+			renderArgs.put("visitedProject", BeanProvider.getProjectService().loadProject(vp, true));
+		}
+		
+		Utils.addAllPossibleLanguageNamesToRenderArgs(getSessionWrapper(), renderArgs);
+		
 		render();
 	}
 	
--- public/javascripts/projects/projectsView.js
+++ public/javascripts/projects/projectsView.js
@@ -1,3 +1,19 @@
 function init() {
+
+	
+	// this is present in languages.js
+	initAllPossibleLanguagesMap();
+	
+	insertLangugeName();
+	
+	
+}
+
+
+function insertLangugeName() {
+	
+	var languageCode = $(".languageCell").find("span.hidden").text();
+	$(".languageCell span.langugeLabel").text(allPossibleLanguagesMap[languageCode]);
+
 	
 }
